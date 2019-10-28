253c3c9757dffa0be9b5e1675dcb1e115cf32431
高翔 <gaoxiang.ian@gmail.com>
2019-10-28 23:56:12 +0800

    部分修改

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module reverse-proxy
 
 go 1.13
+
+require github.com/gin-gonic/gin v1.4.0
--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"net/http/httputil"
+	"net/url"
 
+	"github.com/gin-gonic/gin"
+)
+
+// ReverseProxy ...
+func ReverseProxy(target string) gin.HandlerFunc {
+	url, err := url.Parse(target)
+	if err != nil {
+		panic(err)
+	}
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	return func(c *gin.Context) {
+		proxy.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+func main() {
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
